044cfc057c690f5026a79b6262ade12ca9a69f75
VictoriaGri95 <v.grivusevich95@gmail.com>
2025-08-30 15:13:56 +0300

    boo-to-word.js

---
+++ 8kyu/boo-to-word.js
@@ -0,0 +1,13 @@
+//❓Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
+
+//🌀TESTS
+// assert.strictEqual(boolToWord(true), 'Yes')
+// assert.strictEqual(boolToWord(false), 'No')
+
+//✅SOLUTION
+function boolToWord( bool ){
+  return  bool ? 'Yes' : 'No';
+}
+
+console.log(boolToWord(false));
+console.log(boolToWord(true));
