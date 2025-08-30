ac0896e42a2626142874a0cf4934576cce5bdfc8
VictoriaGri95 <v.grivusevich95@gmail.com>
2025-08-30 14:31:40 +0300

    find-average.js

---
+++ 8kyu/find-average.js
@@ -0,0 +1,22 @@
+//❓DESCRIPTION
+//Write a function which calculates the average of the numbers in a given array.
+//
+// Note: Empty arrays should return 0.
+
+//🌀TESTS
+//tester([1,1,1], 1);
+//tester([1,2,3], 2);
+//tester([1,2,3,4], 2.5);
+
+//✅SOLUTION
+function findAverage(array) {
+  let sum = 0;
+  if (array.length === 0) {
+    return 0;
+  } else {
+for (let i = 0; i < array.length; i++) {
+sum += array[i];
+}
+return sum / array.length;
+  }
+}
