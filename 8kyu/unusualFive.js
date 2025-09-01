3ad81854b75b0a52204bbb33b54390c06ae46c93
VictoriaGri95 <v.grivusevich95@gmail.com>
2025-09-01 22:32:08 +0300

    unusualFive.js

---
+++ 8kyu/unusualFive.js
@@ -0,0 +1,18 @@
+//❓DESCRIPTION
+// Write a function that always returns 5
+//
+// Sounds easy right? Just bear in mind that you can't use any of the following characters: 0123456789*+-/
+
+
+
+//🌀TESTS
+//  assert.strictEqual(unusualFive(), 5);
+
+
+//✅SOLUTION
+function unusualFive() {
+  return 'five!'.length;
+
+}
+
+console.log(unusualFive())
